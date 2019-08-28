94c60b86568ad5406f700537eef1b0a3b22401cc
bxchen <z64471517@qq.com>
2019-08-28 23:33:12 +0800

    Complete food detail.

--- src/components/food/food.vue
+++ src/components/food/food.vue
@@ -31,6 +31,37 @@
         <split></split>
         <div class="rating">
           <h1 class="title">商品评价</h1>
+          <ratingselect
+            :ratings="food.ratings"
+            :selectType="selectType"
+            :onlyContent="onlyContent"
+            :desc="desc"
+            v-on:changeType="changeType($event)"
+            v-on:changeOnlyContent="changeOnlyContent($event)"
+          ></ratingselect>
+          <div class="rating-list">
+            <ul v-show="food.ratings">
+              <li
+                v-show="need(rating.rateType,rating.text)"
+                v-for="(rating,index) in food.ratings"
+                class="rating-item border-1px"
+                :key="index"
+              >
+                <div class="user">
+                  <span class="name">{{ rating.username }}</span>
+                  <img :src="rating.avatar" class="avatar" width="12" height="12" />
+                </div>
+                <div class="time">{{ rating.rateTime || formatDate }}</div>
+                <p class="text">
+                  <span
+                    :class="{'icon-thumb_up': rating.rateType===0,'icon-thumb_down': rating.rateType===1}"
+                  ></span>
+                  {{ rating.text }}
+                </p>
+              </li>
+            </ul>
+            <div class="no-rating" v-show="!food.ratings">暂无评价</div>
+          </div>
         </div>
       </div>
     </div>
@@ -40,9 +71,11 @@
 <script>
 import Vue from 'vue';
 import BScroll from 'better-scroll';
+import {formatDate} from '../../common/js/date';
 import cartControl from '../cartcontrol/cartcontrol';
 import ratingselect from '../ratingselect/ratingselect';
 import split from '../split/split';
+const ALL = 2;
 export default {
   props: {
     food: {
@@ -51,7 +84,14 @@ export default {
   },
   data() {
     return {
-      showDrag: false
+      showDrag: false,
+      selectType: ALL,
+      onlyContent: false,
+      desc: {
+        all: '全部',
+        positive: '推荐',
+        negative: '吐槽'
+      }
     };
   },
 
@@ -60,11 +100,18 @@ export default {
     split,
     ratingselect
   },
-
+  filters: {
+    formatDate(time) {
+      let date = new Date(time);
+      return formatDate(date, 'yyyy-MM-dd hh:mm');
+    }
+  },
   methods: {
     show() {
       this.showDrag = true;
       this.$nextTick(() => {
+        this.selectType = ALL;
+        this.onlyContent = false;
         if (!this.scroll) {
           this.scroll = new BScroll(this.$refs.foodDetail, {
             click: true
@@ -82,13 +129,31 @@ export default {
         return false;
       }
       Vue.set(food, 'count', 1);
+    },
+    changeType(newValue) {
+      this.$nextTick(() => {
+        this.selectType = newValue;
+      });
+    },
+    changeOnlyContent(newValue) {
+      this.$nextTick(() => {
+        this.onlyContent = newValue;
+      });
+    },
+    need(type, text) {
+      if (this.onlyContent && !text) {
+        return false;
+      } else if (this.selectType === ALL) {
+        return true;
+      } else {
+        return type === this.selectType;
+      }
     }
   }
 };
-
 </script>
 <style lang='scss' scoped>
-@import "../../common/css/index";
+@import '../../common/css/index';
 .food-detail-wrapper {
   position: fixed;
   top: 0;
@@ -201,13 +266,66 @@ export default {
       }
     }
     .rating {
-      padding: 18px;
+      padding-top: 18px;
       .title {
         font-size: 14px;
         font-weight: 700;
-        margin-bottom: 6px;
+        margin: 0 0 6px 18px;
         line-height: 14px;
         color: rgb(7, 17, 27);
+      }
+      .rating-list {
+        padding: 18px;
+        .rating-item {
+          position: relative;
+          padding: 16px 0;
+          @include border-1px(rgba(7, 17, 27, 0.1));
+          .user {
+            position: absolute;
+            right: 0;
+            top: 16px;
+            line-height: 12px;
+            font-size: 0;
+            .name {
+              display: inline-block;
+              margin-right: 6px;
+              vertical-align: top;
+              font-size: 10px;
+              color: rgb(147, 153, 159);
+            }
+            .avatar {
+              border-radius: 50%;
+            }
+          }
+          .time {
+            margin-bottom: 6px;
+            line-height: 12px;
+            font-size: 10px;
+            color: rgb(147, 153, 159);
+          }
+          .text {
+            line-height: 16px;
+            font-size: 12px;
+            color: rgb(7, 17, 27);
+            .icon-thumb_up,
+            .icon-thumb_down {
+              margin-right: 4px;
+              line-height: 16px;
+              font-size: 12px;
+            }
+            .icon-thumb_up {
+              color: rgb(0, 160, 220);
+            }
+            .icon-thumb_down {
+              color: rgb(147, 153, 159);
+            }
+          }
+        }
+        .no-rating {
+          padding: 16px 0;
+          font-size: 12px;
+          color: rgb(147, 153, 159);
+        }
       }
     }
   }
--- src/components/ratingselect/ratingselect.vue
+++ src/components/ratingselect/ratingselect.vue
@@ -1,12 +1,30 @@
 <template>
   <div class="ratingselect">
-
+    <div class="rating-type border-1px">
+      <span class="desc positive" :class="{'active': ratingType === 2}" @click="select(2,$event)">
+        {{ desc.all }}
+        <span class="count">{{ ratings.length }}</span>
+      </span>
+      <span class="desc positive" :class="{'active': ratingType === 0}" @click="select(0,$event)">
+        {{ desc.positive }}
+        <span class="count">{{ positive.length }}</span>
+      </span>
+      <span class="desc negative" :class="{'active': ratingType === 1}" @click="select(1,$event)">
+        {{ desc.negative }}
+        <span class="count">{{ negative.length }}</span>
+      </span>
+    </div>
+    <div class="switch" :class="{'on': isContent === true}" @click="toggleContent($event)">
+      <span class="icon-check_circle"></span>
+      <span class="text">只看有内容的评价</span>
+    </div>
+    <div></div>
   </div>
 </template>
 
 <script>
 const POSITIVE = 0;
-const NATIVE = 1;
+const NEGATIVE = 1;
 const ALL = 2;
 export default {
   props: {
@@ -30,21 +48,106 @@ export default {
         return {
           all: '全部',
           positive: '满意',
-          native: '不满意'
+          negative: '不满意'
         };
       }
     }
   },
   data() {
     return {
+      ratingType: this.selectType,
+      isContent: this.onlyContent
     };
   },
-
-  components: {},
-
-  methods: {}
+  computed: {
+    positive() {
+      return this.ratings.filter((rating) => {
+        return rating.rateType === POSITIVE;
+      });
+    },
+    negative() {
+      return this.ratings.filter((rating) => {
+        return rating.rateType === NEGATIVE;
+      });
+    }
+  },
+  methods: {
+    select(type, event) {
+      if (!event._constructed) {
+        return false;
+      }
+      this.ratingType = type;
+      this.$emit('changeType', type);
+    },
+    toggleContent(event) {
+      if (!event._constructed) {
+        return false;
+      }
+      this.isContent = !this.isContent;
+      this.$emit('changeOnlyContent', this.isContent);
+    }
+  }
 };
-
 </script>
 <style lang='scss' scoped>
+@import '../../common/css/index';
+.ratingselect {
+  .rating-type {
+    padding: 12px 0 18px;
+    margin: 0 18px;
+    @include border-1px(rgba(7, 17, 27, 0.1));
+    font-size: 0;
+    .desc {
+      display: inline-block;
+      padding: 8px 12px;
+      margin-right: 8px;
+      border-radius: 1px;
+      font-size: 12px;
+      line-height: 16px;
+      color: rgb(77, 85, 93);
+      &.active {
+        color: #fff;
+      }
+      .count {
+        margin-left: 2px;
+        font-size: 8px;
+      }
+      &.positive {
+        background-color: rgba(0, 160, 220, 0.2);
+        &.active {
+          background-color: rgb(0, 160, 220);
+        }
+      }
+      &.negative {
+        background-color: rgba(77, 83, 93, 0.2);
+        &.active {
+          background-color: rgb(77, 83, 93);
+        }
+      }
+    }
+  }
+  .switch {
+    padding: 12px 18px;
+    line-height: 24px;
+    border-bottom: 1px solid rgba(7, 17, 27, 0.1);
+    color: rgb(147, 153, 159);
+    font-size: 0;
+    &.on {
+      .icon-check_circle {
+        color: #00c850;
+      }
+    }
+    .icon-check_circle {
+      display: inline-block;
+      vertical-align: top;
+      margin-right: 4px;
+      font-size: 24px;
+    }
+    .text {
+      display: inline-block;
+      vertical-align: top;
+      font-size: 12px;
+    }
+  }
+}
 </style>
