ef3931e74a3ac0c8c88bd89a5ee0c0f15ef4d7a6
Edward Chen <edwardc@ciandt.com>
2019-08-30 11:59:12 +0800

    Complete ratings.

--- src/components/ratings/ratings.vue
+++ src/components/ratings/ratings.vue
@@ -1,38 +1,73 @@
 <template>
-  <div class="rating">
-    <div class="overview">
-      <div class="overview-left">
-        <div class="score">{{ seller.score }}</div>
-        <div class="zonghe">综合评分</div>
-        <div class="rankRate">高于周边商家{{seller.rankRate}}%</div>
-      </div>
-      <div class="overview-right">
-        <div class="score-wrapper">
-          <span class="title">服务态度</span>
-          <star :size="36" :score="seller.serviceScore" class="star"></star>
-          <span class="score">{{ seller.serviceScore }}</span>
-        </div>
-        <div class="score-wrapper">
-          <span class="title">商品评分</span>
-          <star :size="36" :score="seller.foodScore" class="star"></star>
-          <span class="score">{{ seller.foodScore }}</span>
-        </div>
-        <div class="delivery-wrapper">
-          <span class="title">送达时间</span>
-          <span class="delivery">{{ seller.deliveryTime }}分钟</span>
-        </div>
-      </div>
-    </div>
-    <split></split>
+  <div class="rating-wrapper" ref="ratingWrapper">
     <div class="rating-content">
-      <ratingselect
-        :ratings="ratings"
-        :selectType="selectType"
-        :onlyContent="onlyContent"
-        :desc="desc"
-        v-on:changeType="changeType($event)"
-        v-on:changeOnlyContent="changeOnlyContent($event)"
-      ></ratingselect>
+      <div class="overview">
+        <div class="overview-left">
+          <div class="score">{{ seller.score }}</div>
+          <div class="zonghe">综合评分</div>
+          <div class="rankRate">高于周边商家{{seller.rankRate}}%</div>
+        </div>
+        <div class="overview-right">
+          <div class="score-wrapper">
+            <span class="title">服务态度</span>
+            <star :size="36" :score="seller.serviceScore" class="star"></star>
+            <span class="score">{{ seller.serviceScore }}</span>
+          </div>
+          <div class="score-wrapper">
+            <span class="title">商品评分</span>
+            <star :size="36" :score="seller.foodScore" class="star"></star>
+            <span class="score">{{ seller.foodScore }}</span>
+          </div>
+          <div class="delivery-wrapper">
+            <span class="title">送达时间</span>
+            <span class="delivery">{{ seller.deliveryTime }}分钟</span>
+          </div>
+        </div>
+      </div>
+      <split></split>
+      <div class="rating">
+        <ratingselect
+          :ratings="ratings"
+          :selectType="selectType"
+          :onlyContent="onlyContent"
+          :desc="desc"
+          v-on:changeType="changeType($event)"
+          v-on:changeOnlyContent="changeOnlyContent($event)"
+        ></ratingselect>
+        <div class="rating-list">
+          <ul>
+            <li
+              v-show="need(item.rateType,item.text)"
+              v-for="(item, index) in ratings"
+              :key="index"
+              class="rating border-1px"
+            >
+              <div class="avatar">
+                <img :src="item.avatar" width="28" height="28" />
+              </div>
+              <div class="content">
+                <h1 class="name">{{ item.username }}</h1>
+                <div class="star-wrapper">
+                  <star :size="24" :score="item.score"></star>
+                  <span class="delivery" v-show="item.deliveryTime">{{ item.deliveryTime }}分钟送达</span>
+                </div>
+                <p class="text">{{ item.text }}</p>
+                <div class="recommend-wrapper" v-show="item.recommend">
+                  <span
+                    :class="{'icon-thumb_up': item.rateType===0,'icon-thumb_down': item.rateType===1}"
+                  ></span>
+                  <span
+                    v-for="(recommend,index) in item.recommend"
+                    :key="index"
+                    class="recommend"
+                  >{{ recommend }}</span>
+                </div>
+                <div class="time">{{ item.rateTime | formatDate }}</div>
+              </div>
+            </li>
+          </ul>
+        </div>
+      </div>
     </div>
   </div>
 </template>
@@ -41,6 +76,8 @@
 import star from '../star/star';
 import split from '../split/split';
 import ratingselect from '../ratingselect/ratingselect';
+import { formatDateTime } from '../../common/js/date';
+import BScroll from 'better-scroll';
 const ERROR_OK = 0;
 const ALL = 2;
 export default {
@@ -57,7 +94,12 @@ export default {
       }
     };
   },
-
+  filters: {
+    formatDate(time) {
+      let date = new Date(time);
+      return formatDateTime(date, 'yyyy-MM-dd hh:mm');
+    }
+  },
   components: {
     star,
     split,
@@ -75,16 +117,49 @@ export default {
       response = response.body;
       if (ERROR_OK === response.errno) {
         this.ratings = response.data;
+        this.$nextTick(() => {
+          this._initScroll();
+        });
       };
     }, (response) => {
     });
   },
-  methods: {}
+  methods: {
+    _initScroll() {
+      if (!this.scroll) {
+        this.scroll = new BScroll(this.$refs.ratingWrapper, {
+          click: true
+        });
+      } else {
+        this.scroll.refresh();
+      }
+    },
+    need(type, text) {
+      if (this.onlyContent && !text) {
+        return false;
+      } else if (this.selectType === ALL) {
+        return true;
+      } else {
+        return type === this.selectType;
+      }
+    },
+    changeType(newValue) {
+      this.$nextTick(() => {
+        this.selectType = newValue;
+      });
+    },
+    changeOnlyContent(newValue) {
+      this.$nextTick(() => {
+        this.onlyContent = newValue;
+      });
+    }
+  }
 };
 
 </script>
 <style lang='scss' scoped>
-.rating {
+@import "../../common/css/index";
+.rating-wrapper {
   position: absolute;
   top: 174px;
   bottom: 0;
@@ -161,6 +236,85 @@ export default {
         .delivery {
           margin-left: 12px;
           font-size: 12px;
+          color: rgb(147, 153, 159);
+        }
+      }
+    }
+  }
+  .rating-list {
+    padding: 0 18px;
+    .rating {
+      display: flex;
+      padding: 18px 0;
+      @include border-1px(rgba(7, 17, 27, 0.1));
+      .avatar {
+        flex: 0 0 28px;
+        width: 28px;
+        margin-right: 12px;
+        img {
+          border-radius: 50%;
+        }
+      }
+      .content {
+        position: relative;
+        flex: 1;
+        .name {
+          line-height: 12px;
+          font-size: 10px;
+          color: rgb(7, 17, 27);
+          margin-bottom: 4px;
+        }
+        .star-wrapper {
+          font-size: 0;
+          margin-bottom: 6px;
+          .star {
+            display: inline-block;
+            vertical-align: top;
+            margin-right: 6px;
+          }
+          .delivery {
+            display: inline-block;
+            vertical-align: top;
+            line-height: 12px;
+            font-size: 10px;
+            color: rgb(147, 153, 159);
+          }
+        }
+        .text {
+          line-height: 18px;
+          color: rgb(7, 17, 27);
+          font-size: 12px;
+          margin-bottom: 8px;
+        }
+        .recommend-wrapper {
+          line-height: 16px;
+          font-size: 0;
+          .icon-thumb_up,
+          .icon-thumb_down,
+          .recommend {
+            display: inline-block;
+            margin: 0 8px 4px 0;
+            font-size: 9px;
+          }
+          .icon-thumb_up {
+            color: rgb(0, 160, 220);
+          }
+          .icon-thumb_down {
+            color: rgb(183, 187, 193);
+          }
+          .recommend {
+            padding: 0 6px;
+            border: 1px solid rgba(7, 17, 27, 0.1);
+            border-radius: 1px;
+            color: rgb(147, 153, 159);
+            background: #fff;
+          }
+        }
+        .time {
+          position: absolute;
+          top: 0;
+          right: 0;
+          font-size: 10px;
           color: rgb(147, 153, 159);
         }
       }
